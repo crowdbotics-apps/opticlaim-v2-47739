f289fc0a63a13aab80c0ef1f920b5a7b2ad35cd6
Crowdbotics <team@crowdbotics.com>
2024-04-29 18:02:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "opticlaim-v2-47739",
+  url: "https://opticlaim-v2-47739.botics.co",
+  api: "https://opticlaim-v2-47739.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
